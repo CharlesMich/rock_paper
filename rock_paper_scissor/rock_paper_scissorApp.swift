24438a6bb2c45b8ba1290dd4a826a1d1a470175b
CharlesMich <charleedongo@hotmail.com>
2025-01-05 15:07:54 -0600

    Initial Commit

---
+++ rock_paper_scissor/rock_paper_scissorApp.swift
@@ -0,0 +1,17 @@
+//
+//  rock_paper_scissorApp.swift
+//  rock_paper_scissor
+//
+//  Created by Charles Michael on 1/5/25.
+//
+
+import SwiftUI
+
+@main
+struct rock_paper_scissorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
